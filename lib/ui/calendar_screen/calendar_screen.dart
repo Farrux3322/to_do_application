dfcede5bbe1b8e9409972f5caced19eb8739045d
nurmuxammad <nurmuhammadzoyidov@gmail.com>
2023-07-03 23:32:17 +0500

    calendar changed

--- lib/ui/calendar_screen/calendar_screen.dart
+++ lib/ui/calendar_screen/calendar_screen.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../utils/colors.dart';
 
 class CalendarScreen extends StatefulWidget {
-  CalendarScreen({Key? key}) : super(key: key);
+  const CalendarScreen({Key? key}) : super(key: key);
 
   @override
-  State<CalendarScreen> createState() => _CalendarScreenState();
+  _CalendarScreenState createState() => _CalendarScreenState();
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  late DateTime _selectedDay;
+  late DateTime _focusedDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = DateTime.now();
+    _focusedDay = _selectedDay;
+  }
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = selectedDay;
+      _focusedDay = focusedDay;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,33 +41,42 @@ class _CalendarScreenState extends State<CalendarScreen> {
         ),
         child: Column(
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 300.h),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    DateTime? dateTime = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1991),
-                      lastDate: DateTime(3030),
-                      builder: (BuildContext context, Widget? child) {
-                        return Theme(
-                          data: ThemeData.light().copyWith(
-                            primaryColor: AppColors.C_1253AA,
-                            hintColor: AppColors.C_05243E,
-                            colorScheme: ColorScheme.light(
-                              primary: AppColors.C_1253AA,
-                            ),
-                            dialogBackgroundColor: AppColors.white,
-                          ),
-                          child: child ?? const SizedBox(),
-                        );
-                      },
-                    );
-                  },
-                  child: Text("Click"),
+            SizedBox(height: 100.h),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 11),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [AppColors.white.withOpacity(0.1), AppColors.white.withOpacity(0.4)],
+                    ),
+                  ),
+                  child: TableCalendar(
+                    calendarStyle: CalendarStyle(
+                      defaultTextStyle: TextStyle(color: AppColors.white),
+                      outsideTextStyle: TextStyle(color: AppColors.black),
+                      weekNumberTextStyle: TextStyle(color: AppColors.white),
+                      // todayDecoration: BoxDecoration(color: Colors.transparent,shape: BoxShape.circle),
+                      selectedDecoration: BoxDecoration(color: AppColors.black,shape: BoxShape.circle),
+                      weekendTextStyle: TextStyle(color: AppColors.C_05243E),
+                    ),
+                    availableGestures: AvailableGestures.all,
+                    focusedDay: _focusedDay,
+                    selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+                    firstDay: DateTime.utc(1991, 01, 01),
+                    lastDay: DateTime.utc(3030, 12, 31),
+                    onDaySelected: _onDaySelected,
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                    ),
+                    rowHeight: 30.h,
+                  ),
+
                 ),
               ),
             ),
